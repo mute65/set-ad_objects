88e88834afa1d235a19b2b8242b864d93a7c5e93
mute65 <96795789+mute65@users.noreply.github.com>
2022-01-21 00:27:23 -0700

    extension attributes

--- set-ad_extensionattributes.ps1
+++ set-ad_extensionattributes.ps1
@@ -1,1 +1,14 @@
+<# Target Identity (samAccountName in this case) and update the msDS extension attributes of an
+   Active Directory user object. CSV Format as follows: samAccount,attribute11,attribute12,attribute13 #>
+
+Import-Module ActiveDirectory
+    
+    $Server = "DOMAINCONTROLLER.FQDN"
+
+    Import-Csv "c:\path-to-csv.csv" | foreach-object {
+
+        Set-ADUser -Server $Server -Identity $_.samAccount -Add @{"msDS-cloudExtensionAttribute11"=$_.attribute11; `
+        "msDS-cloudExtensionAttribute12"=$_.attribute12;"msDS-cloudExtensionAttribute13"=$_.attribute13}
+    
+    }
 
